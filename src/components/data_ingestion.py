24dbe5f42e1795dde30b6bdbfdbc63f80decdc20
Shrutika <connectwithshrutip@gmail.com>
2024-01-21 18:35:25 +0530

    data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,6 +14,9 @@ from src.components.data_transformation import DataTransformationConfig
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 
+#from src.components.hyperparam import ModelTrainerConfig_param
+#from src.components.hyperparam import ModelTrainer_param
+
 
 @dataclass
 class DataIngestionConfig:
@@ -55,5 +58,10 @@ if __name__== "__main__":
     train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
 
     modeltrainer = ModelTrainer()
-    print("R2 score is:")
-    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+
+    print("Best R2 score & Best model is:",modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    print("--"*27)
+
+   # modeltrainer_param = ModelTrainer_param()
+    #print("R2 score with hyperparameter tuning: ")
+    #print(modeltrainer_param.initiate_model_trainer_param(train_arr, test_arr))
